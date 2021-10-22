36bf5a4aa772ed90d3aab93b7d099bbe64d67cc7
alebader <92956203+alebader@users.noreply.github.com>
2021-10-21 21:13:18 -0300

    Creacion de springboot project para front end MELI

---
+++ SpringBootMeli/src/main/java/com/springboot/meli/SpringBootMeli/SpringBootMeliApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.meli.SpringBootMeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMeliApplication.class, args);
+	}
+
+}
---
+++ SpringBootMeli/src/test/java/com/springboot/meli/SpringBootMeli/SpringBootMeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.meli.SpringBootMeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
